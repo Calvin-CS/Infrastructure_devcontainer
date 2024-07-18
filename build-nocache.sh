1fece103866857c97296257677b7957c9b94dc09
Chris Wieringa <cwieri39@calvin.edu>
2024-07-18 10:23:04 -0400

    split off to multiple containers per class

--- build-nocache.sh
+++ build-nocache.sh
@@ -4,3 +4,8 @@
 # For github actions, should rely on the Github secrets stuff, adding each one seperately
 
 docker build --no-cache -t devcontainer:latest .
+docker build --no-cache -t devcontainer-cs10x:latest cs10x/.
+docker build --no-cache -t devcontainer-cs112:latest cs112/.
+docker build --no-cache -t devcontainer-cs212:latest cs212/.
+docker build --no-cache -t devcontainer-cs214:latest cs214/.
+docker build --no-cache -t devcontainer-cs262:latest cs262/.
--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@
 # For github actions, should rely on the Github secrets stuff, adding each one seperately
 
 docker build -t devcontainer:latest .
+docker build -t devcontainer-cs10x:latest cs10x/.
+docker build -t devcontainer-cs112:latest cs112/.
+docker build -t devcontainer-cs212:latest cs212/.
+docker build -t devcontainer-cs214:latest cs214/.
+docker build -t devcontainer-cs262:latest cs262/.
