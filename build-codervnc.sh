cd4f4da2a40230658dad47f6617f3cdcb15a10d6
Chris Wieringa <cwieri39@calvin.edu>
2024-07-25 13:03:12 -0400

    removal of codervnc

--- build-codervnc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Note: this relies on Docker secrets to build, but that secret is not stored in Git.  This build script looks up one directory and down into a secrets subdir
-# For github actions, should rely on the Github secrets stuff, adding each one seperately
-
-cd codervnc
-docker buildx build --tag devcontainer-codervnc:latest --build-context codervnc=../ .
--- run-codervnc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it -p 6081:6081 devcontainer-codervnc
